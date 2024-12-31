3b4dd8edde93b755d2b5fee0b0c5206ee79b6b04
Preethu Mary George <preethuedin@gmail.com>
2024-12-31 16:32:05 -0500

    add get status function

--- scrape.py
+++ scrape.py
@@ -11,21 +11,24 @@ LINKEDIN_COOKIE = os.getenv('LINKEDIN_COOKIE')
 
 # Function to start the Phantom
 def start_phantom():
-    url = f"https://api.phantombuster.com/api/v1/agent/{PHANTOM_ID}/launch"
+    url = f"https://api.phantombuster.com/api/v2/agents/launch"
     headers = {
         "Content-Type": "application/json",
         "X-Phantombuster-Key-1": API_KEY,
     }
-    payload = {
+
+    data = {
+        "id":PHANTOM_ID,  
         "argument": {
             "sessionCookie": LINKEDIN_COOKIE,
-            "search": "elna",  # Specify your LinkedIn search query
+            "userAgent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
+            "search": f"https://www.linkedin.com/search/results/people/?activelyHiringForJobTitles=%5B%22535%22%5D&keywords=jagar&origin=FACETED_SEARCH&sid=OEU", 
             "numberOfResultsPerLaunch": 5,
             "numberOfResultsPerSearch": 5,
         },
     }
-    
-    response = requests.post(url, headers=headers, json=payload)
+
+    response = requests.post(url, headers=headers, json=data)
     
     if response.status_code == 200:
         print("Phantom started successfully!")
@@ -33,49 +36,63 @@ def start_phantom():
     else:
         print("Error starting Phantom:", response.text)
         return None
-
-def get_result_file_url():
-    # API endpoint to fetch Phantom details
-    url = f"https://api.phantombuster.com/api/v2/agents/fetch"
+    
+def get_container_status(container_id):
+    url = f"https://api.phantombuster.com/api/v2/containers/fetch?id={container_id}"
 
     headers = {
-        "X-Phantombuster-Key-1": API_KEY,
+        "accept": "application/json",
+        "X-Phantombuster-Key": API_KEY
     }
-    payload = {
-        "id": PHANTOM_ID  # Provide the Phantom ID
+
+    response = requests.get(url, headers=headers)
+
+    return response.json().get('status')
+
+def get_result_object(container_id):
+    # API endpoint to fetch Phantom details
+    url = f"https://api.phantombuster.com/api/v2/containers/fetch-result-object?id={container_id}"
+
+    headers = {
+        "accept": "application/json",
+        "X-Phantombuster-Key": API_KEY,
     }
 
     # Make the API call
-    response = requests.get(url, headers=headers, params=payload)
+    response = requests.get(url, headers=headers)
 
     if response.status_code == 200:
-        
+        return response.json()
         # Extract s3Folder and orgS3Folder
-        s3_folder = response.json().get("s3Folder")
-        org_s3_folder = response.json().get("orgS3Folder")
+        # s3_folder = response.json().get("s3Folder")
+        # org_s3_folder = response.json().get("orgS3Folder")
 
-        if s3_folder and org_s3_folder:
-            # Construct the JSON and CSV URLs
-            json_url = f"https://phantombuster.s3.amazonaws.com/{org_s3_folder}/{s3_folder}/result.json"
-            csv_url = f"https://phantombuster.s3.amazonaws.com/{org_s3_folder}/{s3_folder}/result.csv"
-            return {"JSON": json_url, "CSV": csv_url}
-        else:
-            print("Error: s3Folder or orgS3Folder not found in the response.")
-            return None
+        # if s3_folder and org_s3_folder:
+        #     # Construct the JSON and CSV URLs
+        #     json_url = f"https://phantombuster.s3.amazonaws.com/{org_s3_folder}/{s3_folder}/result.csv"
+        #     return {"JSON": json_url}
+        # else:
+        #     print("Error: s3Folder or orgS3Folder not found in the response.")
+        #     return None
     else:
-        print(f"Error fetching Phantom details: {response.text}")
+        print(f"Error fetching Phantom results: {response.text}")
         return None
 
 # Main Script
 if __name__ == "__main__":
+        import time
     # Start the Phantom
         phantom_data = start_phantom()
-        
-        # # Wait for Phantom to complete execution
-        import time
-        time.sleep(25)
+        container_id = phantom_data.get('containerId')
+        print(container_id)
 
-        result_urls = get_result_file_url()
-        if result_urls:
-            print(f"JSON File: {result_urls['JSON']}")
+        while True:
+            status = get_container_status(container_id)
+            if status == "finished":
+                print("Container Status is finished!")
+                result = get_result_object(container_id).get('resultObject')
+                print(result)
+                break
+            time.sleep(1) 
+    
         
